063d1c85c3a0f3b10825fe4579b6ff2d18929859
muxudiinkeey <57723477+muxudiinkeey@users.noreply.github.com>
2023-01-12 02:14:15 +0100

    waa saxay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import  {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { StarComponent } from './shared/star/star.component';
 import { HomeComponent } from './home/home.component';
 
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './../product';
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-product-list',
